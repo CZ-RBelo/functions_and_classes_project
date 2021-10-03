2c4a79f9fb876ebd9e0a058955a36efd1c0bc49c
CZ-RBelo <jr.belo@hotmail.com>
2021-10-03 15:31:01 +0100

    initial commit

---
+++ app/src/main/java/com/rbelo/functionsandclasses/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.rbelo.functionsandclasses
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+/*    fun helloWorld(view: View)
+    {
+    textViewString
+
+    }*/
+
+   fun makeSimpsons(view: View) {
+
+        val simpson = Simpson(nameText.text.toString(), Integer.parseInt(ageText.text.toString()), textJob.text.toString())
+
+       textViewString.text = "Name: " + simpson.name + "Age: " + simpson.age + "Job: " + simpson.job
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/rbelo/functionsandclasses/Simpson.kt
@@ -0,0 +1,5 @@
+package com.rbelo.functionsandclasses
+
+class Simpson (var name:String, var age:Int, var job:String){
+
+}
